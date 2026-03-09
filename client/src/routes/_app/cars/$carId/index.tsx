66f7e707df8577c22a36817f0e067ecb241a0b37
Tobias Vinther <Tobias.Vinther88@gmail.com>
2026-03-09 08:54:30 +0100

    feat(cars): navigate to detail page with edit mode from car card
    Edit button in car card menu now navigates to the detail page with
    ?edit=true search param, activating edit mode on arrival instead of
    opening a separate modal.

--- client/src/routes/_app/cars/$carId/index.tsx
+++ client/src/routes/_app/cars/$carId/index.tsx
@@ -1,21 +1,26 @@
-import { createFileRoute } from "@tanstack/react-router"
-import { CarDetailPage } from "@/components/cars/car-detail-page"
-import { getUserCarFn } from "@/lib/api/user-cars.server"
-import { getCarImagesFn } from "@/lib/api/car-images.server"
+import { createFileRoute } from "@tanstack/react-router";
+import { CarDetailPage } from "@/components/cars/car-detail-page";
+import { getUserCarFn } from "@/lib/api/user-cars.server";
+import { getCarImagesFn } from "@/lib/api/car-images.server";
+import z from "zod";
+
+const searchSchema = z.object({ edit: z.boolean().optional() });
 
 export const Route = createFileRoute("/_app/cars/$carId/")({
+  validateSearch: searchSchema,
   loader: async ({ params }) => {
-    const carId = Number(params.carId)
+    const carId = Number(params.carId);
     const [car, imagesResponse] = await Promise.all([
       getUserCarFn({ data: { carId } }),
       getCarImagesFn({ data: { carId } }),
-    ])
-    return { car, images: imagesResponse?.images ?? [] }
+    ]);
+    return { car, images: imagesResponse?.images ?? [] };
   },
   component: CarDetailRoute,
-})
+});
 
 function CarDetailRoute() {
-  const { car, images } = Route.useLoaderData()
-  return <CarDetailPage car={car} images={images} />
+  const { car, images } = Route.useLoaderData();
+  const { edit } = Route.useSearch();
+  return <CarDetailPage car={car} images={images} startInEditMode={!!edit} />;
 }
